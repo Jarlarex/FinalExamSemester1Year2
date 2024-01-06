2480688fc996720a5773652b8e330acd89f4b8de
Iarfhlaith Feeney <S00238682@atu.ie>
2024-01-06 11:26:09 +0000

    Question 1(a), (b), (c), (d), (e), (f), (g)

---
+++ BudgetItem.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalExamSemester1Year2
+{
+    public enum BudgetItemType 
+    {
+        Income,
+        Expense
+    }
+
+    class BudgetItem : IComparable<BudgetItem>
+    {
+        public string Name { get; set; }
+        public decimal Amount { get; set; }
+        public BudgetItemType ItemType { get; set; }
+        public DateTime Date { get; set; }
+        public bool Recurring { get; set; }
+
+        public int CompareTo(BudgetItem other)
+        {
+            if (other == null) return 1;
+            return this.Date.Day.CompareTo(other.Date.Day);
+        }
+
+        public override string ToString()
+        {
+            string recurring = Recurring ? "Recurring" : "One Off";
+            return $"{Date.Day} : {Name} €{Amount:f2} - ({recurring})";
+        }
+    }
+
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,11 +18,61 @@ namespace FinalExamSemester1Year2
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
+    //LINK TO GITHUB REPOSITORY: https://github.com/Jarlarex/FinalExamSemester1Year2
+
     public partial class MainWindow : Window
     {
+        //List to hold the budget items
+        private List<BudgetItem> itemType = new List<BudgetItem>();
+
         public MainWindow()
         {
             InitializeComponent();
+            //Initialize and load data when application is started
+            GetData();
+            LoadItems();
+        }
+
+        //Method to populate data
+        public void GetData()
+        {
+            itemType.Add(new BudgetItem { Name = "Grant", Amount = 300, ItemType = BudgetItemType.Income, Date = new DateTime(2024, 1, 5), Recurring = true });
+            itemType.Add(new BudgetItem { Name = "Bonus", Amount = 300, ItemType = BudgetItemType.Income, Date = new DateTime(2024, 1, 15), Recurring = false });
+            itemType.Add(new BudgetItem { Name = "Wages", Amount = 100, ItemType = BudgetItemType.Income, Date = new DateTime(2024, 1, 25), Recurring = true });
+            itemType.Add(new BudgetItem { Name = "Rent", Amount = 400, ItemType = BudgetItemType.Expense, Date = new DateTime(2024, 1, 1), Recurring = true });
+            itemType.Add(new BudgetItem { Name = "Flight", Amount = 100, ItemType = BudgetItemType.Expense, Date = new DateTime(2024, 1, 5), Recurring = false });
+            itemType.Add(new BudgetItem { Name = "Netflix", Amount = 10, ItemType = BudgetItemType.Expense, Date = new DateTime(2024, 1, 15), Recurring = true });
+            itemType.Add(new BudgetItem { Name = "Spotify", Amount = 8, ItemType = BudgetItemType.Expense, Date = new DateTime(2024, 1, 20), Recurring = true });
+            
+
+        }
+
+        //Method to load items to the list boxes
+        private void LoadItems()
+        {
+            lbxIncome.ItemsSource = itemType.Where(i => i.ItemType == BudgetItemType.Income);
+            lbxExpenses.ItemsSource = itemType.Where(i => i.ItemType == BudgetItemType.Expense);
+            decimal totalIncome = itemType.Where(i => i.ItemType == BudgetItemType.Income).Sum(i => i.Amount);
+            lblTotalIncome.Content = $"Total Income: €{totalIncome:f2}";
+            decimal totalExpense = itemType.Where(i => i.ItemType == BudgetItemType.Expense).Sum(i => i.Amount);
+            lblTotalOutgoing.Content = $"Total Outgoings: €{totalExpense:f2}";
+            decimal currentBalance = totalIncome - totalExpense;
+            lblCurrentBalance.Content = $"Current Balance: €{currentBalance:f2}";
+        }
+
+
+
+
+
+        private void btnRemove_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
